f289338dca2953b82a423d43401158d54534f238
islamov-jahja <yahyaislamov15@mail.ru>
2020-06-13 12:07:35 +0300

    инициализация проекта

---
+++ src/main/kotlin/com/natasha/mmzapp/MmzappApplication.kt
@@ -0,0 +1,11 @@
+package com.natasha.mmzapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MmzappApplication
+
+fun main(args: Array<String>) {
+	runApplication<MmzappApplication>(*args)
+}
---
+++ src/test/kotlin/com/natasha/mmzapp/MmzappApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.natasha.mmzapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MmzappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
