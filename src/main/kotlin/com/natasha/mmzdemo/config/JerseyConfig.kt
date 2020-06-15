360b46a5ef6e0af796893d08527b2659495ab1d6
islamov-jahja <yahyaislamov15@mail.ru>
2020-06-15 09:06:08 +0300

    подгтовил jar для выкладки

--- src/main/kotlin/com/natasha/mmzdemo/config/JerseyConfig.kt
+++ src/main/kotlin/com/natasha/mmzdemo/config/JerseyConfig.kt
@@ -1,11 +1,20 @@
 package com.natasha.mmzdemo.config
 
+import com.natasha.mmzdemo.application.controllers.auth.AuthController
 import org.glassfish.jersey.server.ResourceConfig
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider
+import org.springframework.core.type.filter.AnnotationTypeFilter
 import org.springframework.stereotype.Component
+import org.springframework.util.ClassUtils
+import java.util.stream.Collectors
+import javax.ws.rs.Path
+import javax.ws.rs.ext.Provider
+
 
 @Component
 class JerseyConfig() : ResourceConfig() {
     init {
-        packages("com.natasha.mmzdemo.application.controllers")
+        register(AuthController::class.java)
+        //packages("com.natasha.mmzdemo.application.controllers")
     }
 }
