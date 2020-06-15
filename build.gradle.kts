8c27fe0b50152bc5992a142f4620187fb258aca1
islamov-jahja <yahyaislamov15@mail.ru>
2020-06-15 04:44:13 +0300

    нужная версия

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 group = "com.natasha"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_
+java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
 	mavenCentral()
